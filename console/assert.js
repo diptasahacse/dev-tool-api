fb9fb74b5be0875a6d9b90efeb3e246b3eae930c
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-10 02:16:56 +0600

    practice all console api method

---
+++ console/assert.js
@@ -0,0 +1,89 @@
+// ----------Assertion-----------
+
+// const x = 5;
+// const y = 7;
+// const message = "5 should not greater than 7";
+// console.assert(x > y, { x, y, message })
+
+// -------------Clear-------------
+// console.clear()
+
+// -----------Count-------
+// console.count();
+// console.count();
+// console.count();
+// console.count("Hi");
+// console.count();
+// console.count();
+// console.count("Hi");
+// console.count();
+
+// -----------Count reset-------
+// console.count();
+// console.countReset();
+// console.count("Hi")
+// console.countReset('Hi');
+
+// -------------Debug------------
+
+// console.debug('Hi');
+
+// ------------Dir-----------
+
+// console.dir(document.body)
+
+
+// ------------Dirxml-----------
+// console.dirxml(document)
+
+
+// ------------Error-----------
+// console.error("This is an error")
+
+// ----------Group--------------
+
+// const parent = "Bangladesh";
+// console.group(parent);
+// console.info("Khulna");
+// console.info("Dhaka");
+// console.info("Kustia");
+// console.info("Jessore");
+// console.info("Barishal");
+// console.info("Faridpur");
+// console.info("Rajshahi");
+// console.groupEnd(parent);
+
+
+// ----------- Log ------------
+// console.log("hi")
+
+// -----------Table---------
+// const arrr = [{
+//     firstName: "Dipta",
+//     lastName: "Saha"
+// }, {
+//     firstName: "Keya",
+//     lastName: "Saha"
+// }];
+
+// console.table(arrr)
+
+// -----------Time ------------
+// console.time();
+// let x = 0
+// for (let index = 0; index < 1000000000; index++) {
+//     x = x + index;
+
+// }
+// console.timeEnd();
+
+// ---------Trace -----------
+
+// const first = () => { second(); };
+// const second = () => { third(); };
+// const third = () => { fourth(); };
+// const fourth = () => { console.trace(); };
+// first();
+
+// ----------Warning-----------
+// console.warn("Hi")
